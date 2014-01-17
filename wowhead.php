74063e1df9a12d63646a9d5d4fe10aa184793e9b
b2un0 <branko.wilhelm@gmail.com>
2014-01-17 19:03:44 +0100

    ide switch

--- wowhead.php
+++ wowhead.php
@@ -3,25 +3,27 @@
 /**
  * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2013 Branko Wilhelm
+ * @copyright  (c) 2013 - 2014 Branko Wilhelm
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
- 
+
 defined('_JEXEC') or die;
 
-class plgSystemWowhead extends JPlugin {
-	
-	public function onBeforeRender() {
-		if (JFactory::getApplication()->isAdmin()) {
-			return;
-		}
-		
-		JFactory::getDocument()->addScript('//static.wowhead.com/widgets/power.js', 'text/javascript', false, true);
-				
-		$config['colorlinks'] = $this->params->get('colorlinks') ? true : false;
-		$config['iconizelinks'] = $this->params->get('iconizelinks') ? true : false;
-		$config['renamelinks'] = $this->params->get('renamelinks') ? true : false;
-		
-		JFactory::getDocument()->addScriptDeclaration('var wowhead_tooltips=' . json_encode($config) . ';');
+class plgSystemWowhead extends JPlugin
+{
+
+    public function onBeforeRender()
+    {
+        if (JFactory::getApplication()->isAdmin()) {
+            return;
+        }
+
+        JFactory::getDocument()->addScript('//static.wowhead.com/widgets/power.js', 'text/javascript', false, true);
+
+        $config['colorlinks'] = $this->params->get('colorlinks') ? true : false;
+        $config['iconizelinks'] = $this->params->get('iconizelinks') ? true : false;
+        $config['renamelinks'] = $this->params->get('renamelinks') ? true : false;
+
+        JFactory::getDocument()->addScriptDeclaration('var wowhead_tooltips=' . json_encode($config) . ';');
     }
 }
